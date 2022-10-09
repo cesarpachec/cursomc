19ae0f4c8199fedc38a7ea3edcf1f21d929b956b
cesarpachec <cesarpachec@email.com>
2022-10-08 21:30:47 -0300

    Busca de Produtos por nome e categorias

--- src/main/java/com/cesarpacheco/cursomc/domain/Produto.java
+++ src/main/java/com/cesarpacheco/cursomc/domain/Produto.java
@@ -19,7 +19,9 @@ import javax.persistence.OneToMany;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class Produto implements Serializable {
+public class Produto 
+
+	implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
